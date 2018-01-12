77c03f26e95b38ec5b8737bbdb1a1d73c162da14
DESKTOP-LLR832J\WorkPC-4 <flame_xxx@inbox.ru>
2018-01-12 17:13:02 +0300

    log location

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -18,8 +19,9 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Log, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
